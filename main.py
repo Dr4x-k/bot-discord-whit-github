df98ef66f63d6d1fd2fa7f2bc18fe77187971365
Dr4x-k <jabe.4124@gmail.com>
2023-09-26 22:18:26 -0700

    Prueba de bot con everyone

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ import discord
 from discord.ext import commands
 from flask import Flask, request, jsonify
 import asyncio
-# from dotenv import load_dotenv
-
-# load_dotenv()
 
 #  Variables de entorno
 secret = os.environ['token']
 channel_id = os.environ['channel']
 
 intents = discord.Intents.default()
-# intents.message_content = True
 
 client = commands.Bot(command_prefix='$', intents=intents)
-
 app = Flask(__name__)
 
 #  Ruta de inicio
